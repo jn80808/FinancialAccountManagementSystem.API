f210c59d49cedc8288aa99f1940d5f29664158ee
JN <joyng808@gmail.com>
2025-02-11 23:13:59 +0800

    [JN] - Add DTO

--- FinancialAccountManagement.API/Model/DTO/AccountDTO.cs
+++ FinancialAccountManagement.API/Model/DTO/AccountDTO.cs
@@ -2,5 +2,10 @@
 {
     public class AccountDTO
     {
+        public int Id { get; set; }
+        public string AccountNumber { get; set; } = string.Empty;
+        public string AccountHolder { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+        public List<TransactionDTO> Transactions { get; set; } = new();
     }
-}
+
--- FinancialAccountManagement.API/Model/DTO/TransactionCreateDTO.cs
+++ FinancialAccountManagement.API/Model/DTO/TransactionCreateDTO.cs
@@ -1,6 +1,11 @@
 ﻿namespace FinancialAccountManagement.API.Model.DTO
 {
-    public class TransactionCreateDTO
+    public class TransactionDTO
     {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public string TransactionType { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
     }
 }
